a12a81deed97d9417d284a9999132eaf3d42bc40
gabde002@fiu.edu <gabde002@fiu.edu>
2022-05-13 23:01:19 -0400

    Wrot a new quote to txt file using file writer

--- get-quote.py
+++ get-quote.py
@@ -1,18 +1,35 @@
 import random
 def main():
-  print("Keep it logically awesome")
+  # print("Keep it logically awesome")
 
- 
+
 
   #open quotes.txt file
+  # f = open("quotes.txt")
+  # quotes = f.readlines()
+  # last = len(quotes)-1
+  # rnd = random.randint(0, last)
+  # f.close()
+
+  # print(quotes[0])
+  # print(quotes[-1])
+  # print(quotes[rnd])
+
+  #print out multiple quotes
+
+  # for quote in quotes:
+  #   print(quote.strip())
+
+
+  #Write to a file
+  f = open("quotes.txt","a")
+  f.write("Cats are awesome and so are you\n")
+  f.close()
+
   f = open("quotes.txt")
   quotes = f.readlines()
-  last = len(quotes)-1
-  rnd = random.randint(0, last)
-  f.close()
-  print(quotes[0])
+
   print(quotes[-1])
-  print(quotes[rnd])
 
 
 if __name__== "__main__":

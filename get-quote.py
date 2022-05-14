eeb3ff57ab3162d26ee5b536e1dfea7bef953b23
gabde002@fiu.edu <gabde002@fiu.edu>
2022-05-13 22:46:59 -0400

    Randomly generate a number and printed the element at that random number index

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,19 @@
-def hello():
+import random
+def main():
   print("Keep it logically awesome")
 
+ 
+
   #open quotes.txt file
-  # f = open("quotes.txt")
-  # quotes = f.readlines()
-  # f.close()
-  # print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  last = len(quotes)-1
+  rnd = random.randint(0, last)
+  f.close()
+  print(quotes[0])
+  print(quotes[-1])
+  print(quotes[rnd])
 
 
-if __name__== "__hello__":
-  hello()
+if __name__== "__main__":
+  main()
